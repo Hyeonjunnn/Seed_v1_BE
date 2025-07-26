ad90083506433fe62c0ee8eed5b339b9b21d4bbb
Hyeonjunnn <dev_sklg0602@naver.com>
2025-07-26 13:19:46 +0900

    25.07.26 init commit

---
+++ src/main/java/com/hyeonjunnn/seed_v1_be/SeedV1BeApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonjunnn.seed_v1_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedV1BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedV1BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyeonjunnn/seed_v1_be/SeedV1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonjunnn.seed_v1_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedV1BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
