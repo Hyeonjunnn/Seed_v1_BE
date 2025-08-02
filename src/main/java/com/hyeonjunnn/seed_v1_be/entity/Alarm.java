e94d92947077a50a38f466fb91553f03abd01c83
Hyeonjunnn <dev_sklg0602@naver.com>
2025-08-02 12:55:56 +0900

    25.08.02 Entity에 데이터 관련 어노테이션 추가

--- src/main/java/com/hyeonjunnn/seed_v1_be/entity/Alarm.java
+++ src/main/java/com/hyeonjunnn/seed_v1_be/entity/Alarm.java
@@ -9,6 +9,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
@@ -16,6 +21,11 @@ import java.time.LocalDateTime;
 import static java.lang.Boolean.FALSE;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "alarm")
 public class Alarm extends BaseEntity {
     @Id
--- src/main/java/com/hyeonjunnn/seed_v1_be/entity/Comment.java
+++ src/main/java/com/hyeonjunnn/seed_v1_be/entity/Comment.java
@@ -9,8 +9,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "comment")
 public class Comment extends BaseEntity {
     @Id
